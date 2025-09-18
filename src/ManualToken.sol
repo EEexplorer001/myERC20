fbfac7b8d2f3b0bf1343dd2cb5b3608b3667a537
Yumeng Zhang <yumengzhang@192.168.1.5>
2025-09-19 02:12:54 +0800

    first commit

---
+++ src/ManualToken.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract ManualToken{
+
+    mapping (address => uint256) private s_balances;
+
+    function name() public pure returns (string memory) {
+        return "ManualToken";
+    }
+
+    function totalSupply() public pure returns (uint256) {
+        return 100 ether; 
+    }
+
+    function decimals() public pure returns (uint8) {
+        return 18;
+    }
+
+    function balanceOf(address account) public view returns (uint256) {
+        return s_balances[account]; 
+    }
+}
